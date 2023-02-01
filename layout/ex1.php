4080b6896067ca6d091add9023251e5df912d73a
udayanga22000 <118618291+udayanga22000@users.noreply.github.com>
2023-02-01 11:17:02 +0200

    exercise 1

--- layout/ex1.php
+++ layout/ex1.php
@@ -12,5 +12,31 @@ echo "Hellow world ! My name is \"David\"";
 ?>
 <h3>3.3 Write the PHP code in to display the current date.</h3>
 <?php echo date("d.m.Y") ?>
+<h3>3.4 PHP is interesting”. </h3>
+<?php
+$title1 = "PHP is interesting";
+echo "<h1>" . $title1 . "</h1>";
+?>
+<h3>3.5 Table and Variables</h3>
+<?php
+$g1 = 5;
+$g2 = 4;
+$g3 = 5;
+echo "
+<table>
+<tr>
+<th> S.N </th><th> Name </th><th> Grade </th>
+</tr>
+<tr>
+<td> 1 </th><th> Pekka </th><th> $g1 </td>
+</tr>
+<td> 2 </th><th> Johanna </th><th> $g2 </td>
+</tr>
+<td> 3 </th><th> John </th><th> $g3 </td>
+</tr>
+</table>
+";
+
+?>
 <?php include "footer.php" ?>
 

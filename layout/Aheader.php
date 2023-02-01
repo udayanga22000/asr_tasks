853034c4e730dc5526022843599b122a83e0f9cc
udayanga22000 <118618291+udayanga22000@users.noreply.github.com>
2023-02-01 14:44:01 +0200

    Task 1
    In-class Task 01.02.2023 (index.php)

---
+++ layout/Aheader.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Contact Project@Team 18</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="style-asiri.css">
+
+</head>
+<body>
+  <div class="container">
+    <header class="site_header">
+        <div class="wrapper"></div>
+        <nav>
+
+            <ul>
+                    <li><a href="#"><i><b>Twinkling Stars<br>Kindergarten</a></i></b></li>
+                    <img src="star.png" class="rounded" alt="Logo" width="100" height="75">
+                    <li><a href="indexkushani.html">Home</a></li>
+                <li><a href="activitiesneda.html">Activities</a></li>
+                <li><a href="contactasiri.html">Contact</a></li>
+                <a class="btn btn-danger" href="enrollsonali.html" role="button">Enroll Today</a>
+            </ul>
+            <li><a href="ex1.php">Go Back to Exercise 1</a></li>
+                </nav>
+
+</header>
